09b410ad71c0d4b44ba4b2e59d8b0a2212ab2e27
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2022-01-04 21:56:09 -0500

    quick details card

---
+++ WeighDown/Client/Shared/Competitions/CompetitionQuickDetailsCard.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using WeighDown.Shared.Models;
+
+namespace WeighDown.Client.Shared.Competitions
+{
+    [Authorize]
+    public partial class CompetitionQuickDetailsCard
+    {
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+
+        [Parameter]
+        public Competition Competition { get; set; }
+    }
+}
