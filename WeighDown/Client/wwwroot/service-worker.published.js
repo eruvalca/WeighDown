a1b220e8984857e885a995139728d388e89bbf72
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2022-02-15 23:13:59 -0500

    added favicons, logos, etc

--- WeighDown/Client/wwwroot/service-worker.published.js
+++ WeighDown/Client/wwwroot/service-worker.published.js
@@ -5,7 +5,13 @@ self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+self.addEventListener('message', event => {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
 
+const CACHE_VERSION = 0.01;
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
