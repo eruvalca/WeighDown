1f5f9513d76c44cb90a9b85ef55c1d5885e2939a
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2022-02-24 09:39:50 -0500

    - test new update mechanism

--- WeighDown/Client/wwwroot/service-worker.published.js
+++ WeighDown/Client/wwwroot/service-worker.published.js
@@ -5,13 +5,8 @@ self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
-self.addEventListener('message', event => {
-    if (event.data.action === 'skipWaiting') {
-        self.skipWaiting();
-    }
-});
 
-const CACHE_VERSION = 0.19;
+const CACHE_VERSION = 0.20;
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
@@ -19,6 +14,9 @@ const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
+
+    // Activate the new service worker as soon as the old one is retired.
+    self.skipWaiting();
 
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
