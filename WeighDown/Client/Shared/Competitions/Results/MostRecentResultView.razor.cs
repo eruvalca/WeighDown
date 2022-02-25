ec47ab2d919c6e208bb28639518e5ea365979101
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2022-02-25 17:15:52 -0500

    - refactored most recent result set view into own component

---
+++ WeighDown/Client/Shared/Competitions/Results/MostRecentResultView.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using WeighDown.Shared;
+using WeighDown.Shared.Models;
+
+namespace WeighDown.Client.Shared.Competitions.Results
+{
+    [Authorize]
+    public partial class MostRecentResultView
+    {
+        [Parameter]
+        public KeyValuePair<WeighInDeadline, List<ContestantResultSet>> MostRecentResultSet { get; set; }
+    }
+}
