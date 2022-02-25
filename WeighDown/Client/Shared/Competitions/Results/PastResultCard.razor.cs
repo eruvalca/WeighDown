10ebc5f8d4c979c9a65bff4d9ecaa5f258df9229
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2022-02-25 17:11:08 -0500

    - moved results components into own folder

---
+++ WeighDown/Client/Shared/Competitions/Results/PastResultCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using WeighDown.Shared;
+using WeighDown.Shared.Models;
+using System.Linq;
+
+namespace WeighDown.Client.Shared.Competitions.Results
+{
+    [Authorize]
+    public partial class PastResultCard
+    {
+        [Parameter]
+        public KeyValuePair<WeighInDeadline, List<ContestantResultSet>> Results { get; set; }
+    }
+}
